3ed5c50f6b004e853374bdd614d80155a91dbf69
Vincenzo Provenza <vichi89@hotmail.it>
2013-07-16 15:53:49 +0200

    Fix Bug

--- src/Zendcart/Factory/ZendCartFactory.php
+++ src/Zendcart/Factory/ZendCartFactory.php
@@ -12,8 +12,9 @@ class ZendCartFactory implements FactoryInterface
         $allServices = $servicelocator->getServiceLocator();
         $config = $allServices->get('ServiceManager')->get('Configuration');
 
-        if (! isset($config['zendcart'])) {
-            throw new \Exception('Configurazione ZendCart non impostata.');
+        if (!isset($config['zendcart']))
+        {
+            throw new \Exception('Configuration ZendCart not set.');
         }
         return new ZendCart(array(
             'vat' => $config['zendcart']['vat']
